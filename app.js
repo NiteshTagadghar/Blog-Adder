ec390074a969c806667d3c5026c8000ee1ca730e
NiteshTagadghar <52933246+NiteshTagadghar@users.noreply.github.com>
2021-11-15 12:30:12 +0530

    server port updated

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get("/posts/:postName", function (req, res) {
     }
   });
 });
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000 ");
+const port = process.env.PORT;
+app.listen(port, function () {
+  console.log("Server started on port ", port);
 });
